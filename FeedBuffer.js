63b051ce6e92f1f808ecf680227d0efbb7cb6133
Ken Colton <kcolton@gmail.com>
2011-11-15 02:02:14 -0500

    License modifications

--- FeedBuffer.js
+++ FeedBuffer.js
@@ -1,14 +1,22 @@
 /**
- * Copyright (c) Ken Colton, 2011
+ * Copyright (c) 2011 Ken Colton
  *
- * Released under GPL V3 Licence. See GPL-LICENSE.txt for more information
- *
- * THIS CODE AND INFORMATION ARE PROVIDED "AS IS" WITHOUT WARRANTY OF ANY 
- * KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A
- * PARTICULAR PURPOSE.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * License available in GPL-LICENSE.txt or <http://www.gnu.org/licenses/>.
  */
-
+ 
+/**
+ * Class for maintaining a buffer of tweets and distributing them
+ */
 function FeedBuffer(parent)
 {
     var _this = this;
